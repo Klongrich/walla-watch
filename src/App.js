09115d48803387f471d79614b6cd34510bf4a682
klongrich <longrichk@gmail.com>
2021-09-22 05:10:38 -0400

    first push

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+
+
+function App() {
+  return (
+    <>
+      <h2>Walla-Watch!</h2>
+
+      <h3>Recently Listed Wallas</h3>
+
+      <div Style="border: 1px solid black;">
+        <p>Filler</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
